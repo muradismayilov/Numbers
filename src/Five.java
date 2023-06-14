e3b9374b893cb5dc30e88c42febb6aaabb651a2a
Murad <muradismayilov83@gmail.com>
2023-06-14 17:29:45 +0400

    Update Five.java

--- src/Five.java
+++ src/Five.java
@@ -1,13 +1,22 @@
+import java.util.Scanner;
+
 public class Five {
     public static void main(String[] args) {
 
-        int x = 4654;
-        int b = x / 10 % 10;
-        int c = x / 100 % 10;
-        int e = x / 1000 % 10;
-        int d = x % 10;
-        if (d == e && b == c) {
+        int y,z=0;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Sayi girin: ");
+        int x = sc.nextInt();
+        int m=x;
+        while (x > 0) {
+            y = x % 10;
+            z = z * 10 + y;
+            x = x / 10;
+        }
+        if (m == z) {
             System.out.println("Sayi polindromdur");
         } else System.out.println("Sayi polindrom deyil");
     }
+
+
 }
